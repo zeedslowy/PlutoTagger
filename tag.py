9736dd08293653eed92f368b6975e977fb15543f
ᴛʜᴇ ᴢᴇᴇᴅ <octokardlooy@gmx.us>
2024-02-04 00:35:50 +0300

    Update tag.py

--- tag.py
+++ tag.py
@@ -9,6 +9,11 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram.types import CallbackQuery
 from config import *
 import yt_dlp
+
+from telethon import events
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from youtube_search import YoutubeSearch
 import os,sys,re,requests
 import asyncio,time
@@ -354,7 +359,76 @@ async def admins(client, message):
       text2 += f"✅ | **Toplam Yöneticiler Listesi**"  
       await teletips.send_message(message.chat.id, text2)           
   except FloodWait as e:
-    await asyncio.sleep(e.value)       
+    await asyncio.sleep(e.value)
+
+
+spam_chats = []
+
+
+@Mukesh.on(events.NewMessage(pattern="^/utag ?(.*)"))
+@Mukesh.on(events.NewMessage(pattern="^@btag ?(.*)"))
+async def mentionall(event):
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond(
+            "__This command can be use in groups!__"
+        )
+
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if isinstance(
+            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.respond("__Only admins can mention all!__")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.respond("__Give me one argument!__")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond(
+                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__"
+            )
+    else:
+        return await event.respond(
+            "__Reply to a message or give me some text to mention others!__"
+        )
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{msg}\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(3)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+
+❍ /tagall or @all '(reply to message or add another message.'
+"""
 
 @Mukesh.on_message(filters.command("bot"))
 async def bots(client, message):  
